37f3d7369773c79b5e3d5243da4340708385c54f
ruthmesfun <ruth.mesfun@gmail.com>
2018-04-24 17:53:09 -0400

    created the method student_username

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ActiveRecord::Base
     has_many :answers
     belongs_to :student
+
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :link, presence: true
+    validates :lab, presence: true
+
+    def student_username
+        self.student.username
+    end
 end
